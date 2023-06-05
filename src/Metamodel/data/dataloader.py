719a527ce3e96f149e8c43cadcefe4378bf0687a
sunghwanism <97149338+sunghwanism@users.noreply.github.com>
2023-06-05 13:37:32 +0900

    update dataloader (window)

--- src/Metamodel/data/dataloader.py
+++ src/Metamodel/data/dataloader.py
@@ -11,7 +11,7 @@ import pandas as pd
 
 class applewatch:
 
-    def __init__(self, database, batch_size, meta_train_client_idx_lst=None):
+    def __init__(self, database, length, meta_train_client_idx_lst=None,):
         
         self.x_data = []
         self.y_data = []
@@ -23,28 +23,53 @@ class applewatch:
             PATH = os.path.join(database, f"c{client}_data.csv")
             data = pd.read_csv(PATH)
             
-            HR = data["heart_rate"]
-            activity = data["step_count"]
-            stage = data["psg_status"].astype(int)
+            for k in range(int(len(data)/length)):
+                front_idx = int(k*length)
+                post_idx = int((k+1)*length)
+                
+                temp = data[front_idx:post_idx]                
+                x_move = temp["x_move"].to_numpy()
+                y_move = temp["y_move"].to_numpy()
+                z_move = temp["z_move"].to_numpy()
+                HR = temp["heart_rate"].to_numpy()
+                activity = temp["step_count"].to_numpy()
+                stage = temp["psg_status"].to_numpy()[0].astype(int)
+                if stage not in [0, 1, 2, 3, 5]:
+                    print(client, stage)
+                
+                self.x_data.append(np.stack([x_move, y_move, z_move, HR, activity], axis=1))
+                self.y_data.append(stage)
         
-            self.x_data.append(np.stack([HR, activity], axis=1))
-            self.y_data.append(np.array(stage))
-            
-        print("Data loaded")
+        self.x_data = np.array(self.x_data)
+        self.y_data = np.array(self.y_data)
+        
+        self.x_data = torch.FloatTensor(self.x_data)
+        self.y_data = torch.FloatTensor(self.y_data).long()
+        
+        self.x_data = self.x_data.permute(0, 2, 1)
+        
+        print("x_data shape: ", self.x_data.shape)
+        print("y_data shape: ", self.y_data.shape, "class_num", self.y_data.unique().shape[0])
         
     def __len__(self):
         return len(self.x_data)
     
     def __getitem__(self, idx):
         
-        return torch.FloatTensor(self.x_data[idx]), torch.Tensor(self.y_data[idx]).long()
+        return self.x_data[idx], self.y_data[idx]
 
 
-def create_train_val_loader(database, batch_size, ):
+def create_train_val_loader(database, batch_size, length):
     
+    print("#######################################")
+    print("Train DataLoader")
+    train_dataset = applewatch(database=database, length=length)
     
-    train_dataset = applewatch(database=database, batch_size=batch_size)
-    valid_dataset = applewatch(database=database, batch_size=batch_size, meta_train_client_idx_lst=[30, 31])
+    print("#######################################")
+    
+    print("Validation DataLoader")
+    valid_dataset = applewatch(database=database, length=length, meta_train_client_idx_lst=[30, 31])
+    print("#######################################")
     
     train_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
     valid_dataloader = DataLoader(valid_dataset, batch_size=batch_size, shuffle=False)
