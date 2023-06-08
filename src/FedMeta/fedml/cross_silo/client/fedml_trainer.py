d0afe9c5de1d2531df06f26da6daf1e2ffad9fae
SHMoon <msh2044@khu.ac.kr>
2023-06-09 05:25:51 +0900

    add test function in fedml_trainer.py

--- src/FedMeta/fedml/cross_silo/client/fedml_trainer.py
+++ src/FedMeta/fedml/cross_silo/client/fedml_trainer.py
@@ -69,6 +69,7 @@ class FedMLTrainer(object):
 
         self.trainer.on_before_local_training(self.train_local, self.device, self.args)
         self.trainer.train(self.train_local, self.device, self.args)
+        self.trainer.test(self.test_local, self.device, self.args)
         self.trainer.on_after_local_training(self.train_local, self.device, self.args)
 
         MLOpsProfilerEvent.log_to_wandb({"Train/Time": time.time() - tick, "round": round_idx})
