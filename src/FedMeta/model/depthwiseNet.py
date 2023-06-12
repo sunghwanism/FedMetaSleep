fa9b547b9e179e99c276fb78236e16fdf4264292
SHMoon <msh2044@khu.ac.kr>
2023-06-12 10:03:12 +0900

    change config for FL

--- src/FedMeta/model/depthwiseNet.py
+++ src/FedMeta/model/depthwiseNet.py
@@ -24,33 +24,48 @@ class MyModule2(nn.Module):
     
     
 class DepthNet(nn.Module):
-    def __init__(self, lengths=30, patch_size=30, in_chans=5, embed_dim=256, norm_layer=None, output_dim=3):
+    def __init__(self, lengths=30, patch_size=1, in_chans=5, embed_dim=256, norm_layer=None, output_dim=3):
         print("DepthNet is used...")
         super().__init__()
-
+        #num_patches = num_voxels // patch_size
+        #self.patch_shape = patch_size
+        #self.num_voxels = num_voxels
+        #self.patch_size = patch_size
+        #self.num_patches = num_patches
         self.lengths = lengths
         self.in_chans = in_chans
         self.embed_dim = embed_dim
+        # self.temporal_embed_1 = nn.Conv1d(in_chans, 8*in_chans, kernel_size=9, stride=2, groups=in_chans, )
+        # self.temporal_embed_2 = nn.Conv1d(8*in_chans, 64*in_chans, kernel_size=9, stride=2, groups=in_chans,)
+        # self.temporal_embed_3 = nn.Conv1d(64*in_chans, 128*in_chans, kernel_size=9, stride=2, groups=in_chans)
+        # self.proj = nn.Conv1d(64, embed_dim, kernel_size=patch_size, stride=patch_size)
+        # self.relu = torch.nn.ReLU()
         self.C = in_chans
             
         self.features = torch.nn.Sequential(
-            nn.Conv1d(in_chans, 16*in_chans, kernel_size=7, stride=1, groups=in_chans,),
-            nn.ReLU(),
-            nn.Conv1d(16*in_chans, 128*in_chans, kernel_size=7, stride=1, groups=in_chans,),
-            nn.ReLU(),
+            nn.Conv1d(in_chans, 64*in_chans, kernel_size=3, stride=1, groups=in_chans,),
+            nn.ELU(),
+            torch.nn.Dropout(0.5),
+            nn.Conv1d(64*in_chans, 256*in_chans, kernel_size=3, stride=1, groups=in_chans,),
+            nn.ELU(),
+            torch.nn.Dropout(0.5),
+            nn.Conv1d(256*in_chans, 512*in_chans, kernel_size=3, stride=1, groups=in_chans,),
+            nn.ELU(),
             MyModule(self.C),
-            nn.Conv1d(128, embed_dim, kernel_size=patch_size, stride=patch_size),
+            nn.Conv1d(512, embed_dim, kernel_size=patch_size, stride=patch_size),
             MyModule2(),
             # nn.LayerNorm(embed_dim)
         )
         
         self.classifier = torch.nn.Sequential(
-            torch.nn.Linear(768, 128), # length=30 768  // length=10 256
+            torch.nn.Linear(256, 128), # patch_size=10 (2304), patch_size=15 (1536), patch_size=30 (768)
             torch.nn.BatchNorm1d(128),
-            torch.nn.ReLU(),
+            torch.nn.ELU(),
+            torch.nn.Dropout(0.5),
             torch.nn.Linear(128, 64),
             torch.nn.BatchNorm1d(64),
-            torch.nn.ReLU(),
+            torch.nn.ELU(),
+            torch.nn.Dropout(0.5),
             torch.nn.Linear(64, output_dim)
         )
     
@@ -70,3 +85,21 @@ class DepthNet(nn.Module):
                     m.reset_parameters()
                 except AttributeError:
                     pass
+                
+                
+        # def forward(self, x,):
+    #     B, C, L = x.shape # batch, channel, lengths
+    #     x = F.relu(self.temporal_embed_1(x))
+    #     x = F.relu(self.temporal_embed_2(x))
+    #     # x = F.relu(self.temporal_embed_3(x))
+        
+    #     x_ = torch.split(x, x.shape[1]//C, dim=1)
+    #     x_ = torch.cat([x_[i] for i in range(C)], dim=-1) # B x 64 x 12*28
+        
+    #     x = self.proj(x_)
+    #     x = x.transpose(1, 2).contiguous() # B x 28 x 512
+        
+    #     if self.norm is not None:
+    #         x = self.norm(x)
+    #     x = self.classifier(x.view(B, -1))
+    #     return x
